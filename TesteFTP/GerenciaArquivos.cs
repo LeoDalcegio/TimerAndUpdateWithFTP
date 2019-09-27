c0578148277025377cd48d2fb85fd765cbe4c689
LeoDalcegio <LeoDalcegio@users.noreply.github.com>
2019-09-27 20:40:12 -0300

    FTP

--- TesteFTP/GerenciaArquivos.cs
+++ TesteFTP/GerenciaArquivos.cs
@@ -21,26 +21,37 @@ namespace TesteFTP
             Arquivos = Directory.GetFiles(_dados.LocalDownload);
 
             foreach (string arq in Arquivos) {
-                if(ComparaArquivos(arq) = true) {
+                if (ComparaArquivos(arq)) {
                     ArqABaixar.Add(arq);
                 }
 
             }
 
             return ArqABaixar;
-            
-            // foreach _dados.LocalDownload
-            // compara se existe em _dados.BaixarPara
-            // horario alteração diferente, adiciona em Arquivos
+
         }
 
         private bool ComparaArquivos(string arqAtual) {
-            //verifica se o arqAtual existe na outra pasta
+     
+            string arq = _dados.BaixarPara + '/' + Path.GetFileNameWithoutExtension(arqAtual);
+            if (System.IO.File.Exists(arq)){
+                if (VerificaAlteracao(arq, )) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+            return false;
+            //verifica se o arqAtual existe na outra pasta (_dados.BaixarPara)
+            // horario alteração diferente, true, adiciona em ArqBaixar
+        }
+
+        private bool VerificaAlteracao(string caminhoArqServ, string caminhoArqLocal) {
 
         }
 
         // método que retorna list com quais arq eu devo baixar
-
         // pegar todos os "arq" do _dados.ArquivoDownload(mudar nome)
         // com as extensões escolhidas e comparar as datas de cada
     }

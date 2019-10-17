c33ddb2593a5c90d539175073f56c7b93d2234b6
LeoDalcegio <LeoDalcegio@users.noreply.github.com>
2019-10-17 06:53:39 -0300

    FTP

--- TesteFTP/ArqsEDiretorios/JSON.cs
+++ TesteFTP/ArqsEDiretorios/JSON.cs
@@ -15,17 +15,18 @@ namespace TesteFTP.ArqsEDiretorios
         }
 
         public JSON() {
-            BuscaDadosTela();
+            
         }
 
 
         public void SalvaDadosTela() {
-
+            // busca _dados e salva num JSON, colocar em outra classe para salvar, funcoesJSON e JSON
+            // colocar em outra classe "Util" o diretorio do salvar json e dir bkp
         }
 
         public DadosUsuario BuscaDadosTela() {
             
-
+            // busca no dir JSON e retorna DadosUsuario
             //loop no arqJSON
             return dadosBkp;
         }
